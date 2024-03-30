354e8ff754b8c5b22b52da42c9e684bafadb710c
Yash Mistry <yash.mistry.g43@gmail.com>
2024-03-31 03:17:20 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,52 +1,69 @@
-# Import of all the library used in this project
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, jsonify
+from threading import Thread
+import time
 
-
-# Configure app
 app = Flask(__name__)
 
-# Configuration Of Auto Reload Of All The Templates
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+signals = ['red', 'green', 'yellow']
+timer_intervals = {
+    'position1': {
+        # Kartik_0_07
+        'latitude': 26.76574685515445,
+        'longitude': 75.8388288617248,
+        'signal': {'red': 75, 'green': 25, 'yellow': 4}
+    },
+    'position2': {
+        # signal 2
+        'latitude': 26.768929548794457,
+        'longitude': 75.84698090363155,
+        'signal': {'red': 60, 'green': 30, 'yellow': 5}
+    },
+    'position3': {
+        # signal 3
+            'latitude': 26.77053657260424,
+            'longitude': 75.85310081235693,
+            'signal': {'red': 40, 'green': 20, 'yellow': 2}
+    }
+}
+
+current_signal = 'red'
+current_signal1 = 'red'
+current_signal2 = 'red'
+remaining_time = timer_intervals['position1']['signal'][current_signal]
+remaining_time1 = timer_intervals['position2']['signal'][current_signal1]
+remaining_time2 = timer_intervals['position3']['signal'][current_signal2]
 
 
-
-@app.after_request
-def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-
-@app.route("/")
-def index():
-    return render_template('api_test_add.html')
-
-
-@app.route("/add", methods=['POST'])
-def add():
-    if request.method == 'POST':
-        f_name = request.form.get("f_name")
-        l_name = request.form.get("l_name")
-        email = request.form.get("email")
-        password = request.form.get("password")
-        params = {"f_name": f_name, "l_name": l_name,
-                  "email": email, "password": password}
-        post_url = requests.post(
-            'http://coderashapi.mywebcommunity.org/data_add.php', data=params)
-        response = post_url.json()
-        messages = response["message"]
-    return render_template('api_test_add.html', response=messages)
+def update_signal():
+    global current_signal,current_signal1,current_signal2,remaining_time,remaining_time1,remaining_time2
+    while True:
+        remaining_time -= 1
+        remaining_time1 -= 1
+        remaining_time2 -= 1
+        if remaining_time <= 0:
+            current_signal = signals[(signals.index(current_signal) + 1) % len(signals)]
+            remaining_time = timer_intervals['position1']['signal'][current_signal]
+        if remaining_time1 <= 0:
+            current_signal1 = signals[(signals.index(current_signal1) + 1) % len(signals)]
+            remaining_time1 = timer_intervals['position2']['signal'][current_signal1]
+        if remaining_time2 <= 0:
+            current_signal2 = signals[(signals.index(current_signal2) + 1) % len(signals)]
+            remaining_time2 = timer_intervals['position3']['signal'][current_signal2]
+        time.sleep(1)
 
 
-@app.route("/fetch")
-def fetch():
-    post_url = requests.get(
-        'http://coderashapi.mywebcommunity.org/data_fetch.php')
-    response = post_url.json()
-    message = response["message"]
-    records = {"data": response['data'][::2]}
+update_thread = Thread(target=update_signal)
+update_thread.daemon = True
+update_thread.start()
 
-    return render_template('api_test_fetch.html', data=records, response=message)
+
+@app.route('/traffic-signal')
+def traffic_signal():
+    return jsonify({'position1':{'latitude': 26.76574685515445,'longitude': 75.8388288617248,'signal': current_signal,'remaining_time':remaining_time},
+                    'position2':{ 'latitude': 26.768929548794457,'longitude': 75.84698090363155,'signal': current_signal1, 'remaining_time': remaining_time1},
+                    'position3': {'latitude': 26.77053657260424,'longitude': 75.85310081235693,'signal': current_signal2, 'remaining_time': remaining_time2},
+                    })
+
+if __name__ == '__main__':
+   app.run(debug=True, host="0.0.0.0")
+
